ec1e11b53ce421a8e287030fb627d5208316a11c
Arun Sivaramakrishnan <arun.sivaramakrishnan@gmail.com>
2016-09-12 12:53:42 +0530

    fix legendre form and change_basis methods

--- src/change_basis.rs
+++ src/change_basis.rs
@@ -7,77 +7,70 @@ use bezier::Bezier;
 use class_invariant::ClassInvariant;
 
 
-pub fn   to_monomial<P:VectorSpace>( bezf : &Bezier<P> ) -> MonomialForm<P>
-{
+pub fn to_monomial<P: VectorSpace>(bezf: &Bezier<P>) -> MonomialForm<P> {
     debug_assert!(bezf.is_valid().is_ok());
     let b = bezf.control_points();
     let sz = b.len();
 
     let mut monf = vec![P::splat(0.0); sz];
     let mut cni = 1;
-   
+
     let n = sz - 1;
-    let mut signi : i32 = -1;
-    for i in 0..sz
-    {
-        signi  = -signi;
+    let mut signi: i32 = -1;
+    for i in 0..sz {
+        signi = -signi;
 
         if i != 0 {
-            cni  = cni*(n - i + 1);
-            cni  = cni / i;
+            cni *= n - i + 1;
+            cni /= i;
         }
-        let mut cil : usize = 1;
+        let mut cil: usize = 1;
 
         let mut signl = -1;
-        for l in 0.. (i+1)
-        {
+        for l in 0..(i + 1) {
             signl = -signl;
             if l != 0 {
-                cil = cil * (i - l + 1);
-                cil = cil/l;
+                cil *= i - l + 1;
+                cil /= l;
             }
             let mut co = (cni * cil) as f64;
-            if signi * signl < 0 
-            {
+            if signi * signl < 0 {
                 co = -co;
             }
-            monf[i] = b[l] * co;
+            monf[i] = monf[i] + b[l] * co;
         }
     }
-    let (s,e) = bezf.param_range();
+    let (s, e) = bezf.param_range();
     MonomialForm::new(monf, s, e)
 }
 
-pub fn   to_bezier<P:VectorSpace>( mf : &MonomialForm<P>) -> Bezier<P>
-{
+pub fn to_bezier<P: VectorSpace>(mf: &MonomialForm<P>) -> Bezier<P> {
     let mflen = mf.len();
     let mut cpts = vec![P::splat(0.0); mflen];
     let n = mflen - 1;
     for l in 0..mflen {
-        let mut clk : usize = 1;
-        let mut cnk : usize = 1;
-        for k in 0..l+1 {
+        let mut clk: usize = 1;
+        let mut cnk: usize = 1;
+        for k in 0..l + 1 {
             if k != 0 {
-                cnk *= n - k +1;
+                cnk *= n - k + 1;
                 cnk /= k;
                 clk *= l - k + 1;
                 clk /= k;
             }
-            cpts[l] = cpts[l] +  mf.point(k) * ((clk as f64) / (cnk as f64));
+            cpts[l] = cpts[l] + mf.point(k) * ((clk as f64) / (cnk as f64));
         }
     }
-    let (s,e) = mf.param_range();
+    let (s, e) = mf.param_range();
     Bezier::new(cpts, s, e)
 }
 
 #[inline]
-fn ncks(n: usize)-> Vec<usize> 
-{
-    let mut ncks_ : Vec<usize> = Vec::with_capacity(n+1);
-    let mut val: usize  = 1;
+fn ncks(n: usize) -> Vec<usize> {
+    let mut ncks_: Vec<usize> = Vec::with_capacity(n + 1);
+    let mut val: usize = 1;
     ncks_.push(1);
-    for k in 1..n+1
-    {
+    for k in 1..n + 1 {
         val *= n - k + 1;
         val /= k;
         ncks_.push(val);
@@ -87,13 +80,11 @@ fn ncks(n: usize)-> Vec<usize>
 
 // return C^{n+k}_k values for k in [0,j]
 #[inline]
-fn nkcks(n:usize, j:usize) ->Vec<usize>
-{
-    let mut nkcks_ : Vec<usize> = Vec::with_capacity(j+1);
-    let  mut val = 1;
+fn nkcks(n: usize, j: usize) -> Vec<usize> {
+    let mut nkcks_: Vec<usize> = Vec::with_capacity(j + 1);
+    let mut val = 1;
     nkcks_.push(1);
-    for k in 1 ..j+1
-    {
+    for k in 1..j + 1 {
         val *= n + k;
         val /= k;
         nkcks_.push(val);
@@ -102,159 +93,174 @@ fn nkcks(n:usize, j:usize) ->Vec<usize>
 }
 
 
-pub fn  to_legendre<P:VectorSpace>(bezf : &Bezier<P>) -> LegendreForm<P>
-{
-    let b   = bezf.control_points();
-    let sz  = (bezf.degree() + 1) as usize;
-    
-    let n    = bezf.degree() as usize;
+pub fn to_legendre<P: VectorSpace>(bezf: &Bezier<P>) -> LegendreForm<P> {
+    let b = bezf.control_points();
+    let n = bezf.degree() as usize;
 
-    let mut legf: Vec<P> = vec![P::splat(0.0); sz];
+    let mut legf: Vec<P> = vec![P::splat(0.0); n + 1];
+    let njcns: Vec<usize> = nkcks(n, n);
 
-    let njcns : Vec<usize> = nkcks(n,n);
-    for k in 0 .. n+1 {
-        let nkjicji:Vec<usize> = nkcks(n-k, n);
-        let kicis: Vec<usize>  = nkcks(k,n);
-        let mut jsign: i32 =  -1;
-        for j in 0 ..n+1
-        {
-            jsign *=- 1;
-            let f = (2.0* (j as f64) + 1.0).sqrt()/((n + j + 1) as f64);
-            let mut mjk:f64  = 0.0;
-            let mut isign:i32 = -1;
-            let mut jci:usize  = 1;
-            for  i in 0..(j+1)  {
+    for k in 0..(n + 1) {
+        let nkjicji: Vec<usize> = nkcks(n - k, n);
+        let kicis: Vec<usize> = nkcks(k, n);
+        let mut jsign: i32 = -1;
+        for j in 0..n + 1 {
+            jsign *= -1;
+            let f = (2.0 * (j as f64) + 1.0).sqrt() / ((n + j + 1) as f64);
+            let mut mjk: f64 = 0.0;
+            let mut isign: i32 = -1;
+            let mut jci: usize = 1;
+            for i in 0..(j + 1) {
                 isign *= -1;
-                if i!= 0 {
-                    jci  *= j - i + 1;
+                if i != 0 {
+                    jci *= j - i + 1;
                     jci /= i;
                 }
-                let m = (jci * kicis[i] * nkjicji[j-i]) as f64;
-                mjk += if isign > 0  {m} else {-m};
+                let m = (jci * kicis[i] * nkjicji[j - i]) as f64;
+                mjk += if isign > 0 {
+                    m
+                } else {
+                    -m
+                };
             }
             mjk /= njcns[j] as f64;
-            mjk *= if jsign > 0 { f } else{ -f };
-            let legfj =  legf[j] + b[k] * mjk;
+            mjk *= if jsign > 0 {
+                f
+            } else {
+                -f
+            };
+            let legfj = legf[j] + b[k] * mjk;
             legf[j] = legfj;
         }
     }
-  
-    let (s,e) = bezf.param_range();
-    LegendreForm::new(legf,s,e)
+    let (s, e) = bezf.param_range();
+    LegendreForm::new(legf, s, e)
 }
 
-pub fn  from_legendre<P:VectorSpace>(lf: &LegendreForm<P>) -> Bezier<P>
-{
-    let n  =  lf.len() - 1;
-    let mut ksign =  -1;
-    let ncjs =  ncks(n);
-    let mut cpts : Vec<P> = Vec::with_capacity(n+1);
-    for k in 0..(n+1)
-    {
+pub fn from_legendre<P: VectorSpace>(lf: &LegendreForm<P>) -> Bezier<P> {
+    let n = lf.len() - 1;
+    let mut ksign = -1;
+    let ncjs = ncks(n);
+    let mut cpts: Vec<P> = vec![P::default();n + 1];
+    for k in 0..(n + 1) {
         ksign = -ksign;
-        let f   = (2.0*(k as f64) + 1.0).sqrt();
+        let f = (2.0 * (k as f64) + 1.0).sqrt();
 
-        let kcis  =   ncks(k) ;
-        let nkcjs =   ncks(n-k) ;
-
-        for  j in 0..(n+1)
-        {
-            let mut mjk   = 0.0;
+        let kcis = ncks(k);
+        let nkcjs = ncks(n - k);
+        for j in 0..(n + 1) {
+            let mut mjk = 0.0;
             let mut isign = -1;
             use std::cmp::min;
-            for  i in 0.. min(j+1 ,k+1)
-            {
+            for i in 0..min(j + 1, k + 1) {
                 isign = -isign;
-                if n - k + i >= j 
-                {
+                if n + i >= j + k {
                     let kisq = kcis[i] * kcis[i];
-                    let nkcji = nkcjs[j-i];
+                    let nkcji = nkcjs[j - i];
                     let m = (kisq * nkcji) as f64;
-                    mjk += if isign > 0 { m } else { -m  };
+                    mjk += if isign > 0 {
+                        m
+                    } else {
+                        -m
+                    };
                 }
             }
-            mjk *= if ksign >0 {f } else { -f};
+            mjk *= if ksign > 0 {
+                f
+            } else {
+                -f
+            };
             mjk /= ncjs[j] as f64;
-            let cptsj = cpts[j] + lf.coeffs()[k]*mjk ;
+            let cptsj = cpts[j] + (*lf.coeff(k)) * mjk;
             cpts[j] = cptsj;
         }
     }
-    let (s,e) = lf.param_range();
-    Bezier::new(cpts, s,e)
+    let (s, e) = lf.param_range();
+    Bezier::new(cpts, s, e)
 }
 
-///#[test]
+#[test]
 fn it_works() {
     use curve::Curve;
     use tol::Tol;
 
- //("monomial to bezier") 
+    // ("monomial to bezier")
     {
-        let mut mon:Vec<f64> = Vec::with_capacity(5);
+        println!("conv monomial to bezier");
+        let mut mon: Vec<f64> = Vec::with_capacity(5);
         for i in 0..5 {
             mon.push((i as f64) + 1.0);
         }
-        let  mf = MonomialForm::new(mon.clone(), 0.0, 1.0);
+        let mf = MonomialForm::new(mon.clone(), 0.0, 1.0);
         let bzf = to_bezier(&mf);
         assert!(bzf.eval(0.1).eqres(mf.eval(0.1)));
-        let mf_dual  = to_monomial(&bzf);
-        let cfs      = mf_dual.points();
-        assert!(cfs.len()  == mon.len());
-        for i in 0 ..cfs.len()
-        {
-            println!("c:{:?},m:{:?}",cfs[i], mon[i]);
+        let mf_dual = to_monomial(&bzf);
+        let cfs = mf_dual.points();
+        assert!(cfs.len() == mon.len());
+        for i in 0..cfs.len() {
+            println!("c:{:?},m:{:?}", cfs[i], mon[i]);
             assert!(cfs[i].eqres(mon[i]));
         }
     }
-    //("bezier  to monomial") 
+    // ("bezier  to monomial")
     {
-        let mut c:Vec<f64> = Vec::with_capacity(5);
+        println!("conv bezier to monomial");
+        let mut c: Vec<f64> = Vec::with_capacity(5);
         for i in 0..5 {
-            c.push((i as f64) + 1.0) ;
+            c.push((i as f64) + 1.0);
         }
 
         let bf = Bezier::new(c.clone(), 1.0, 2.0);
-        let bf_dual = to_bezier (&to_monomial(&bf));
-        let  cfs = bf_dual.control_points();
+        let mon = to_monomial(&bf);
+        assert!((mon.eval(0.1) - bf.eval(0.1)).small());
+        println!("{:?},{:?}", mon.eval(0.1), bf.eval(0.1));
+        let bf_dual = to_bezier(&mon);
+        let cfs = bf_dual.control_points();
         assert!(cfs.len() == c.len());
-        for i in 0 ..cfs.len()
-        {
-            assert!(cfs[i] .eqres(c[i]));
+        for i in 0..cfs.len() {
+            assert!(cfs[i].eqres(c[i]));
         }
     }
-    //("legendre to bezier") 
+    // ("legendre to bezier")
     {
-        let mut legf:Vec<f64> = Vec::with_capacity(5);
+        println!("conv legendre to bezier");
+        let mut legf: Vec<f64> = Vec::with_capacity(5);
         for i in 0..5 {
-            legf.push((i as f64) + 1.0 );
+            legf.push((i as f64) + 1.0);
         }
-        let  lf = LegendreForm::new(legf.clone(), 0.0, 1.0);
-        let bzf      = from_legendre(&lf);
+        let lf = LegendreForm::new(legf.clone(), 0.0, 1.0);
+        let bzf = from_legendre(&lf);
+        println!("bz:{:?}", bzf.eval(0.1));
+        println!("lf:{:?}", lf.eval(0.1));
         assert!(bzf.eval(0.1).eqres(lf.eval(0.1)));
+        println!("bz:{:?}", bzf.eval(0.9));
+        println!("lf:{:?}", lf.eval(0.9));
         assert!(bzf.eval(0.9).eqres(lf.eval(0.9)));
-        let lf_dual  = to_legendre(&bzf);
-        let cfs      = lf_dual.coeffs();
-        assert!(cfs.len()  == legf.len());
-        for i in 0 ..cfs.len()
-        {
+        let lf_dual = to_legendre(&bzf);
+        let cfs = lf_dual.coeffs();
+        assert!(cfs.len() == legf.len());
+        for i in 0..cfs.len() {
             assert!(cfs[i].eqres(legf[i]));
         }
     }
-    //("bezier to legendre") 
+    // ("bezier to legendre")
     {
-        let mut c:Vec<f64> = Vec::with_capacity(5);
+        println!("conv bezier to legendre");
+        let mut c: Vec<f64> = Vec::with_capacity(5);
         for i in 0..5 {
-            c.push((i as f64) + 1.0 );
+            c.push((i as f64) + 1.0);
         }
         let bf = Bezier::new(c.clone(), 1.0, 2.0);
-        let  lf = to_legendre(&bf);
-        let  bf_dual = from_legendre(&lf);
+        let lf = to_legendre(&bf);
+        println!("bz:{:?}", bf.eval(0.1));
+        println!("lf:{:?}", lf.eval(0.1));
+        let bf_dual = from_legendre(&lf);
+        println!("bz_dual:{:?}", bf_dual.eval(0.1));
         assert!(bf.eval(1.1).eqres(lf.eval(1.1)));
         assert!(bf.eval(1.9).eqres(lf.eval(1.9)));
-        let   cfs = bf_dual.control_points();
+        let cfs = bf_dual.control_points();
         assert!(cfs.len() == c.len());
-        for i in 0 ..cfs.len()
-        {
+        for i in 0..cfs.len() {
             assert!(cfs[i].eqres(c[i]));
         }
     }
--- src/legendre_form.rs
+++ src/legendre_form.rs
@@ -3,40 +3,44 @@ use curve::{Curve, FiniteCurve};
 
 
 #[derive(Debug)]
-pub struct  LegendreForm<P:VectorSpace> {
-    a : Vec<P>,
+pub struct LegendreForm<P: VectorSpace> {
+    a: Vec<P>,
     s: f64,
     e: f64,
 }
 
-impl<P:VectorSpace> LegendreForm<P>
-{
-    pub fn new(a: Vec<P>, s: f64, e: f64) -> LegendreForm<P>
-    {
-        LegendreForm{a:a, s:s, e:e}
+impl<P: VectorSpace> LegendreForm<P> {
+    pub fn new(a: Vec<P>, s: f64, e: f64) -> LegendreForm<P> {
+        LegendreForm { a: a, s: s, e: e }
     }
-    
+
     pub fn coeffs(&self) -> &Vec<P> {
         &self.a
     }
-    
+
+    pub fn coeff(&self, k: usize) -> &P {
+        &self.a[k]
+    }
+
     pub fn len(&self) -> usize {
         self.a.len()
     }
+
+    pub fn degree(&self) -> usize {
+        self.a.len() - 1
+    }
 }
 
-impl <P:VectorSpace> Curve for LegendreForm<P>
-{
+impl<P: VectorSpace> Curve for LegendreForm<P> {
     type T = P;
 
-    fn eval(&self,u : f64) -> P {
-       self.eval_derivative(u, 0)
+    fn eval(&self, u: f64) -> P {
+        self.eval_derivative(u, 0)
     }
-    
-    fn eval_derivative(&self, u: f64, der_order: u32) -> P
-    {
-        let u = (u - self.s)/(self.e - self.s);
-        let u = 2.0*u  - 1.0; // map (s,e) -> (-1,1)
+
+    fn eval_derivative(&self, u: f64, der_order: u32) -> P {
+        let u = (u - self.s) / (self.e - self.s);
+        let u = 2.0 * u - 1.0; // map (s,e) -> (-1,1)
         let mut p1 = u;
         let mut p0 = 1.0;
         let mut v = P::splat(0.0);
@@ -46,47 +50,58 @@ impl <P:VectorSpace> Curve for LegendreForm<P>
         for i in 2..self.a.len() {
             let id = i as f64;
             // farouki 2000, bernstein to legendre conversion.
-            let pnu = p1 * ((2*i-1) as f64) * u - p0 * (id - 1.0);
+            let pnu = p1 * ((2 * i - 1) as f64) * u - p0 * (id - 1.0);
             coeffs[i] = pnu / id;
             p0 = p1;
-            p1 = pnu; 
+            p1 = coeffs[i];
         }
-        
+
         // use the recursion : D[P_{n+1}(x)] = (2n + 1) P_n(x) +  D[P_{n-1}(x)]
-        // or in general D^j[P_{n+1}(x)] = (2n + 1) D^{j-1}P_n(x) +  D^j[P_{n-1}(x)]  
-        for j in 1..(der_order as usize)
-        {
-            let mut coeffs_der : Vec<f64> = coeffs.clone();
-            coeffs_der[j-1] = 0.0;
+        // or in general D^j[P_{n+1}(x)] = (2n + 1) D^{j-1}P_n(x) +  D^j[P_{n-1}(x)]
+        for j in 1..(der_order as usize) {
+            let mut coeffs_der: Vec<f64> = coeffs.clone();
+            coeffs_der[j - 1] = 0.0;
             for i in j..self.a.len() {
                 let id = i as f64;
-                coeffs_der[i] = (2.0*id - 1.0) * coeffs[i] + coeffs_der[i-1];
+                coeffs_der[i] = (2.0 * id - 1.0) * coeffs[i] + coeffs_der[i - 1];
             }
-            coeffs = coeffs_der.iter().cloned().map(|x| {x*2.0/(self.e-self.s)}).collect(); // account for remapped domain using chain rule
+            coeffs = coeffs_der.iter()
+                               .cloned()
+                               .map(|x| x * 2.0 / (self.e - self.s))
+                               .collect(); // account for remapped domain using chain rule
         }
-        for (&c,&a) in coeffs.iter().zip(self.a.iter())
-        {
-            v = v + a*c;
+        for (i, (&a, &c)) in self.a.iter().zip(coeffs.iter()).enumerate() {
+            v = v + a * c * ((2 * i + 1) as f64).sqrt(); // sqrt(2i+1) makes legendre polynomials orthonormal
         }
         v
     }
 }
 
-impl<P:VectorSpace> FiniteCurve for LegendreForm<P>
-{
-  fn param_range(&self) -> (f64, f64)
-  {
-      (self.s,self.e)
-  }
+impl<P: VectorSpace> FiniteCurve for LegendreForm<P> {
+    fn param_range(&self) -> (f64, f64) {
+        (self.s, self.e)
+    }
 }
 
 #[test]
 fn it_works() {
     use tol::Tol;
-    let lf = LegendreForm::new(vec!(0.0,1.0,2.0), 0., 1.);
+    let lf = LegendreForm::new(vec![1.0, 0.0, 0.0], 0., 1.);
     println!("{:?}", lf.eval(0.0));
     assert!((lf.eval(0.0) - 1.0).len().small());
     println!("{:?}", lf.eval(1.0));
-    assert!((lf.eval(1.0) - 3.0).len().small());
+    assert!((lf.eval(1.0) - 1.0).len().small());
+
+    let lf = LegendreForm::new(vec![0.0, 1.0, 0.0], 0., 1.);
+    println!("{:?}", lf.eval(0.0));
+    assert!((lf.eval(0.0) + (3.0 as f64).sqrt()).len().small());
+    println!("{:?}", lf.eval(1.0));
+    assert!((lf.eval(1.0) - (3.0 as f64).sqrt()).len().small());
+
+    let lf = LegendreForm::new(vec![0.0, 0.0, 1.0], 0., 1.);
+    println!("{:?}", lf.eval(0.0));
+    assert!((lf.eval(0.0) - (5.0 as f64).sqrt()).len().small());
+    println!("{:?}", lf.eval(1.0));
+    assert!((lf.eval(1.0) - (5.0 as f64).sqrt()).len().small());
 
 }
