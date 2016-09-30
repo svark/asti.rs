f4129611d39df9e78795a80df130f86807293e07
Arun Sivaramakrishnan <arun.sivaramakrishnan@gmail.com>
2016-09-30 10:20:14 +0530

    param finder upgraded

--- src/change_basis.rs
+++ src/change_basis.rs
@@ -5,6 +5,7 @@ use vectorspace::VectorSpace;
 use legendre_form::LegendreForm;
 use bezier::Bezier;
 use class_invariant::ClassInvariant;
+use combinations::{ncks,nkcks};
 
 
 pub fn to_monomial<P: VectorSpace>(bezf: &Bezier<P>) -> MonomialForm<P> {
@@ -65,34 +66,7 @@ pub fn to_bezier<P: VectorSpace>(mf: &MonomialForm<P>) -> Bezier<P> {
     Bezier::new(cpts, s, e)
 }
 
-#[inline]
-fn ncks(n: usize) -> Vec<usize> {
-    let mut ncks_: Vec<usize> = Vec::with_capacity(n + 1);
-    let mut val: usize = 1;
-    ncks_.push(1);
-    for k in 1..n + 1 {
-        val *= n - k + 1;
-        val /= k;
-        ncks_.push(val);
-    }
-    ncks_
-}
-
-// return C^{n+k}_k values for k in [0,j]
-#[inline]
-fn nkcks(n: usize, j: usize) -> Vec<usize> {
-    let mut nkcks_: Vec<usize> = Vec::with_capacity(j + 1);
-    let mut val = 1;
-    nkcks_.push(1);
-    for k in 1..j + 1 {
-        val *= n + k;
-        val /= k;
-        nkcks_.push(val);
-    }
-    nkcks_
-}
-
-
+// B.-G. Lee et al. / Computer Aided Geometric Design 19 (2002) 709-718
 pub fn to_legendre<P: VectorSpace>(bezf: &Bezier<P>) -> LegendreForm<P> {
     let b = bezf.control_points();
     let n = bezf.degree() as usize;
